a1ff18b9f0554fd768d20930d84e41122ebdbc35
CharlesCoxC3 <charles.cox.iii@aol.com>
2023-03-06 20:52:49 -0800

    added code for stripe subscriptions

--- community-compass/src/pages/Membership.js
+++ community-compass/src/pages/Membership.js
@@ -15,9 +15,7 @@ function AccountCreationPage() {
     }
 
     const handleSubmitSub = async (event) => {
-        if (!stripe) {
 
-        }
 
     }
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
-const stripe = require('stripe')('sk_test_51Mgh8NGGno84ND8L78IKd03OnvFmjQyoMCj4p3v0MPWOyKMy99wM9CU4HMzZYhGCrISbTVxSGuc7Zb9hnArIl9cc00ct3Tb9VX');
-const priceId = '{{PRICE_ID}}';
-
 const express = require("express");
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+
 
 const { typeDefs, resolvers } = require('./schemas');
 
@@ -22,7 +20,7 @@ const server = new ApolloServer({
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../community-compass/build')));
@@ -45,58 +43,52 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
   };
 
-//Stripe
+  startApolloServer(typeDefs, resolvers);
+
+//Stripe Subscriptions
+
+const stripe = require('stripe')('sk_test_51Mgh8NGGno84ND8L78IKd03OnvFmjQyoMCj4p3v0MPWOyKMy99wM9CU4HMzZYhGCrISbTVxSGuc7Zb9hnArIl9cc00ct3Tb9VX');
+const priceId = '{{PRICE_ID}}';
+
+
 
 app.get("/checkout-session", async (req, res) => {
-  const { sessionId } = req.query;
-  const session = await stripe.checkout.sessions.retrieve(sessionId);
-  res.send(session);
+const { sessionId } = req.query;
+const session = await stripe.checkout.sessions.retrieve(sessionId);
+res.send(session);
 });
 
 app.post("/create-checkout-session", async (req, res) => {
 //  const domainURL = process.env.DOMAIN;
-  const { priceId } = '{{PRICE_ID}}';
+const { priceId } = "{{PRICE_ID}}";
+const prices = await stripe.prices.list({
+    lookup_keys: [req.body.lookup_key],
+    expand: ["data.product"],
+});
+// Create new Checkout Session for the order
 
-  // Create new Checkout Session for the order
-  // Other optional params include:
-  // [billing_address_collection] - to display billing address details on the page
-  // [customer] - if you have an existing Stripe Customer ID
-  // [customer_email] - lets you prefill the email input in the form
-  // [automatic_tax] - to automatically calculate sales tax, VAT and GST in the checkout page
-  // For full details see https://stripe.com/docs/api/checkout/sessions/create
-  try {
+try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: "subscription",
-      line_items: [
+    mode: "subscription",
+    line_items: [
         {
-          price: priceId,
-          quantity: 1,
-
+        price: prices.data[0].id,
+        quantity: 1,
         },
-      ],
-      // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-      success_url: `http://localhost:3000`,
-      cancel_url: `http://localhost:3000/membership`,
-      // automatic_tax: { enabled: true }
+    ],
+    // On Success the user will return here
+    success_url: `http://localhost:3000`,
+    cancel_url: `http://localhost:3000/membership`,
+    // automatic_tax: { enabled: true }
     });
 
     return res.redirect(303, session.url);
-  } catch (e) {
+} catch (e) {
     res.status(400);
     return res.send({
-      error: {
+    error: {
         message: e.message,
-      }
-
+    },
     });
-
-  }
-  
+}
 });
-
-
-
-  startApolloServer(typeDefs, resolvers);
-
-  
